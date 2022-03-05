67d4daf3a91d4c1f7d5ee68e554c1a278c45da4e
Vicente <fc55386@alunos.fcul.ul.pt>
2022-03-05 19:06:12 +0000

    [#5] Initial commit
    Created process.c file

---
+++ src/process.c
@@ -0,0 +1,13 @@
+#include "process.h"
+
+int launch_restaurant(int restaurant_id, struct communication_buffers* buffers, struct main_data* data) {
+}
+
+int launch_driver(int driver_id, struct communication_buffers* buffers, struct main_data* data) {
+}
+
+int launch_client(int client_id, struct communication_buffers* buffers, struct main_data* data) {
+}
+
+int wait_process(int process_id) {
+}
